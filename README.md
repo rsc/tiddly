82185c306b15719bd1ea93feded335baf4df3b8d
Russ Cox <rsc@golang.org>
2016-12-22 23:32:15 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# TiddlyWiki App Engine Server
+
+This is a minimal Google App Engine app, written in Go, that can serve
+as the back end for a personal [TiddlyWiki](http://tiddlywiki.com/)
+
+The [TiddlyWiki5](https://github.com/Jermolene/TiddlyWiki5) implementation
+has a number of back end options. This app implements the backend expected
+by the “[TiddlyWeb](http://tiddlyweb.com/) and TiddlySpace components” plugin.
+
+The usual way to deploy TiddlyWeb is to run a fairly complex Python web server
+program. I'd rather not. Instead I implemented a minimal Go server that responds
+appropriately to the (relatively few) needed JSON API calls.
+
+## Authentication
+
+The TiddlyWeb JSON API envisions a multiuser system in which different users have
+access to different sets of tiddlers. This Go server contains none of that:
+it assumes that all users have full access to everything, although it does record
+who created which tiddlers. The only access control is that the app.yaml here
+requires HTTPS and administrator login for all URLs, and as a “belt and suspenders” measure,
+the app itself also refuses to serve to non-admins, as checked by user.IsAdmin.
+
+## Data model
+
+The app stores the current tiddlers in Cloud Datastore as Tiddler entities.
+It also stores every version of every tiddler as TiddlerHistory entities.
+Currently nothing reads the TiddlerHistory, but in case of a mistake that
+wipes out important Tiddler contents it should be possible to reconstruct
+lost data from the TiddlerHistory.
+
+The TiddlyWiki downloaded as index.html that runs in the browser
+downloads (through the JSON API) a master list of all tiddlers and their
+metadata when the page first loads and then lazily fetches individual 
+tiddler content on demand.
+
+## Deployment
+
+Create an Google App Engine standard app and deploy with
+
+	appcfg.py -A your-app -V your-version update .
+
+Then visit https://your-app.appspot.com/. As noted above, only admins
+will have access to the content.
+
+## Plugins
+
+TiddlyWiki supports extension through plugins. 
+Plugins need to be in the downloaded index.html, not lazily
+like other tiddlers. Therefore, installing a plugin means 
+updating a local copy of index.html and redeploying it to
+the server.
+
+## Macros
+
+TiddlyWiki allows tiddlers with the tag `$:/tags/Macro` to contain
+global macro definitions made available to all tiddlers.
+The lazy loading of tiddler bodies interferes with this: something
+has to load the tiddler body before the macros it contains take effect.
+To work around this, the app includes the body of all macro tiddlers
+in the initial tiddler list (which otherwise does not contain bodies).
+This is sufficient to make macros take effect on reload.
+
+For some reason, no such special hack is needed for `$:/tags/Stylesheet` tiddlers.
+
+## Synchronization
+
+If you set Control Panel > Info > Basics > Default tiddlers by clicking
+“retain story ordering”, then the story list (the list of tiddlers shown on the page)
+is written to the server as it changes and is polled back from the server every 60 seconds.
+This means that if you have the web site open in two different browsers 
+(for example, at home and at work), changes to what you're viewing in one
+propagate to the other.
+
+## TiddlyWiki base image
+
+The TiddlyWiki code is stored in and served from index.html, which
+(as you can see by clicking on the Tools tab) is TiddlyWiki version 5.1.13.
+
+Plugins must be pre-baked into the TiddlyWiki file, not stored on the server
+as lazily loaded Tiddlers. The index.html in this directory is 5.1.13 with
+the TiddlyWeb and Markdown plugins added. The TiddlyWeb plugin is
+required, so that index.html talks back to the server for content.
+
+The process for preparing a new index.html is:
+
+- Open tiddlywiki-5.1.13.html in your web browser.
+- Click the control panel (gear) icon.
+- Click the Plugins tab.
+- Click "Get more plugins".
+- Click "Open plugin library".
+- Type "tiddlyweb" into the search box. The "TiddlyWeb and TiddlySpace components" should appear.
+- Click Install. A bar at the top of the page should say "Please save and reload for the changes to take effect."
+- Click the icon next to save, and an updated file will be downloaded.
+- Open the downloaded file in the web browser.
+- Repeat, adding any more plugins.
+- Copy the final download to index.html.
+
---
+++ tiddly.go
@@ -0,0 +1,246 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tiddly
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/user"
+)
+
+func init() {
+	http.HandleFunc("/", authCheck(main))
+	http.HandleFunc("/status", authCheck(status))
+	http.HandleFunc("/recipes/all/tiddlers/", authCheck(tiddler))
+	http.HandleFunc("/recipes/all/tiddlers.json", authCheck(tiddlerList))
+	http.HandleFunc("/bags/bag/tiddlers/", authCheck(deleteTiddler))
+}
+
+func authCheck(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := appengine.NewContext(r)
+		u := user.Current(ctx)
+		if u == nil || !user.IsAdmin(ctx) {
+			http.Error(w, "permission denied", 403)
+			return
+		}
+		f(w, r)
+	}
+}
+
+type Tiddler struct {
+	Rev  int    `datastore:"Rev,noindex"`
+	Meta string `datastore:"Meta,noindex"`
+	Text string `datastore:"Text,noindex"`
+}
+
+func main(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "bad method", 405)
+		return
+	}
+	if r.URL.Path != "/" {
+		http.Error(w, "not found", 404)
+		return
+	}
+
+	http.ServeFile(w, r, "index.html")
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "bad method", 405)
+		return
+	}
+	ctx := appengine.NewContext(r)
+	w.Header().Set("Content-Type", "application/json")
+	u := user.Current(ctx)
+	name := "GUEST"
+	if u != nil {
+		name = u.String()
+	}
+	w.Write([]byte(`{"username": "` + name + `", "space": {"recipe": "all"}}`))
+}
+
+func tiddlerList(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	q := datastore.NewQuery("Tiddler")
+	// Only need Meta, but get no results if we do this.
+	if false {
+		q = q.Project("Meta")
+	}
+	it := q.Run(ctx)
+	var buf bytes.Buffer
+	sep := ""
+	buf.WriteString("[")
+	for {
+		var t Tiddler
+		_, err := it.Next(&t)
+		if err != nil {
+			if err == datastore.Done {
+				break
+			}
+			println("ERR", err.Error())
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		if len(t.Meta) == 0 {
+			continue
+		}
+		meta := t.Meta
+
+		// Tiddlers containing macros don't take effect until
+		// they are loaded. Force them to be loaded by including
+		// their bodies in the skinny tiddler list.
+		// Might need to expand this to other kinds of tiddlers
+		// in the future as we discover them.
+		if strings.Contains(meta, `"$:/tags/Macro"`) {
+			var js map[string]interface{}
+			err := json.Unmarshal([]byte(meta), &js)
+			if err != nil {
+				continue
+			}
+			js["text"] = string(t.Text)
+			data, err := json.Marshal(js)
+			if err != nil {
+				continue
+			}
+			meta = string(data)
+		}
+
+		buf.WriteString(sep)
+		sep = ","
+		buf.WriteString(meta)
+	}
+	buf.WriteString("]")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(buf.Bytes())
+}
+
+func tiddler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getTiddler(w, r)
+	case "PUT":
+		putTiddler(w, r)
+	default:
+		http.Error(w, "bad method", 405)
+	}
+}
+
+func getTiddler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	title := strings.TrimPrefix(r.URL.Path, "/recipes/all/tiddlers/")
+	key := datastore.NewKey(ctx, "Tiddler", title, 0, nil)
+	var t Tiddler
+	if err := datastore.Get(ctx, key, &t); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	var js map[string]interface{}
+	err := json.Unmarshal([]byte(t.Meta), &js)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	js["text"] = string(t.Text)
+	data, err := json.Marshal(js)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func putTiddler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	title := strings.TrimPrefix(r.URL.Path, "/recipes/all/tiddlers/")
+	key := datastore.NewKey(ctx, "Tiddler", title, 0, nil)
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "cannot read data", 400)
+		return
+	}
+	var js map[string]interface{}
+	err = json.Unmarshal(data, &js)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	js["bag"] = "bag"
+
+	rev := 1
+	var old Tiddler
+	if err := datastore.Get(ctx, key, &old); err == nil {
+		rev = old.Rev + 1
+	}
+	js["revision"] = rev
+
+	var t Tiddler
+	text, ok := js["text"].(string)
+	if ok {
+		t.Text = text
+	}
+	delete(js, "text")
+	t.Rev = rev
+	meta, err := json.Marshal(js)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	t.Meta = string(meta)
+	_, err = datastore.Put(ctx, key, &t)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	key2 := datastore.NewKey(ctx, "TiddlerHistory", title+"#"+fmt.Sprint(t.Rev), 0, nil)
+	if _, err := datastore.Put(ctx, key2, &t); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	etag := fmt.Sprintf("\"bag/%s/%d:%x\"", url.QueryEscape(title), rev, md5.Sum(data))
+	w.Header().Set("Etag", etag)
+}
+
+func deleteTiddler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	if r.Method != "DELETE" {
+		http.Error(w, "bad method", 405)
+		return
+	}
+	title := strings.TrimPrefix(r.URL.Path, "/bags/bag/tiddlers/")
+	key := datastore.NewKey(ctx, "Tiddler", title, 0, nil)
+	var t Tiddler
+	if err := datastore.Get(ctx, key, &t); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	t.Rev++
+	t.Meta = ""
+	t.Text = ""
+	if _, err := datastore.Put(ctx, key, &t); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	key2 := datastore.NewKey(ctx, "TiddlerHistory", title+"#"+fmt.Sprint(t.Rev), 0, nil)
+	if _, err := datastore.Put(ctx, key2, &t); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
